faf631233d53970acbbb05e43b343e623f530950
Ricky Johnson <ricky.johnson@techsoft3d.com>
2023-01-13 13:35:17 -0500

    updated wss

--- javascripts/conversionService.js
+++ javascripts/conversionService.js
@@ -7,12 +7,17 @@ export async function startViewer(uid) {
 
         let res = await fetch(conversionServiceURI + '/api/streamingSession');
         var data = await res.json();
+        var endpointUriBeginning = 'ws://';
+
+        if(conversionServiceURI.substring(0, 5).includes("https")){
+                endpointUriBeginning = 'wss://'
+        }
 
         await fetch(conversionServiceURI + '/api/enableStreamAccess/' + data.sessionid, { method: 'put', headers: { 'items': JSON.stringify(uid) } });
 
         viewer = new Communicator.WebViewer({
                 containerId: "container",
-                endpointUri: 'wss://' + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
+                endpointUri: endpointUriBeginning + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
                 model: "_empty",
                 enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@latest",
                 rendererType: 0
